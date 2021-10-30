87036fec551cb227b8c2770e7b1abc17d0736dc4
Sam Jones <sam.jones240699@gmail.com>
2021-10-30 22:31:48 +0100

    tidying

--- back/index.js
+++ back/index.js
@@ -4,6 +4,8 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 const idLen = 8;
 const idCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+// Generate a unique identifier for a game
+// This is an 8 digit, mixed case alphanumeric code
 function genID(){
     let out = [];
     for (let i=0;i<idLen;i++){
@@ -13,6 +15,7 @@ function genID(){
 }
 
 exports.handler = async (event) => {
+    // Setting up stuff for response
     let body;
     let statusCode = 200;
     const headers = {
@@ -21,6 +24,7 @@ exports.handler = async (event) => {
     
     try {
         switch (event.routeKey) {
+            // Create a new game, add it to the database, send back the game ID
             case "PUT /games":
                 let game_id = genID();
                 await dynamo
@@ -28,13 +32,19 @@ exports.handler = async (event) => {
                         TableName: "war-web-db",
                         Item: {
                             game_id: game_id,
-                            phase:0,
-                            state:[]
+                            phase: 0,
+                            state: []
                         }
                     })
                     .promise();
                 body = game_id;
                 break;
+
+            // Push a move or board setup to a game
+            case "PUT /games/{id}":
+                break
+            
+            // Return game state for requested ID
             case "GET /games/{id}":
                 body = await dynamo
                     .get({
@@ -48,6 +58,7 @@ exports.handler = async (event) => {
             default:
                 throw new Error(`Unsupported route: "${event.routeKey}"`);
         }
+    // Handle errors by dumping the message in a 400 res
     } catch (err) {
         statusCode = 400;
         body = err.message;
