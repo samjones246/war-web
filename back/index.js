1a70606246455375f5c1376bfcbe2bb53aa6607e
Sam Jones <sam.jones240699@gmail.com>
2021-10-31 16:43:24 +0000

    created initial endpoints

--- back/index.js
+++ back/index.js
@@ -15,10 +15,21 @@ function genID(){
     return out.join("")
 }
 
+async function getGame(id){
+    game = await dynamo.get({
+        TableName: "war-web-db",
+        Key: {
+            game_id: id
+        }
+    }).promise();
+    return game.Item
+}
+
 exports.handler = async (event) => {
     // Setting up stuff for response
     let body;
     let statusCode = 200;
+    let pid;
     const headers = {
         "Content-Type": "application/json"
     };
@@ -28,7 +39,7 @@ exports.handler = async (event) => {
             // Create a new game, add it to the database, send back the game ID and player id
             case "PUT /games":
                 let game_id = genID();
-                let pid = uuidv4();
+                pid = uuidv4();
                 await dynamo
                     .put({
                         TableName: "war-web-db",
@@ -45,20 +56,39 @@ exports.handler = async (event) => {
                         pid: pid};
                 break;
 
+            // Join a game via its ID
+            case "PUT /games/{id}/join":
+                let game = await getGame(event.pathParameters.id)
+                if (game.phase !== 0 || game.pid2 != null){
+                    throw new Error(`This game is already full`)
+                }
+                pid = uuidv4();
+                await dynamo.update({
+                    TableName: "war-web-db",
+                    Key: {
+                        game_id: event.pathParameters.id
+                    },
+                    UpdateExpression: "SET #ID = :id, #P = :p",
+                    ExpressionAttributeNames: {
+                        "#ID": "pid2",
+                        "#P": "phase"
+                    },
+                    ExpressionAttributeValues: {
+                        ":id": pid,
+                        ":p": 1
+                    }
+                }).promise();
+                body = {pid:pid}
+                break;
+
+
             // Push a move or board setup to a game
             case "PUT /games/{id}":
                 break
             
             // Return game state for requested ID
             case "GET /games/{id}":
-                body = await dynamo
-                    .get({
-                        TableName: "war-web-db",
-                        Key: {
-                            game_id: event.pathParameters.id
-                        }
-                    })
-                    .promise();
+                body = await getGame(event.pathParameters.id)
                 break;
             default:
                 throw new Error(`Unsupported route: "${event.routeKey}"`);
